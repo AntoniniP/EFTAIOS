3269d0b8713331e5004560f3c54324a3d7e6dc2f
PaoloAnt <paolo1.antonini@mail.polimi.it>
2015-04-14 17:24:59 +0200

    First attempt.

--- AntoniniCastiglia/src/main/java/it/polimi/ingsw/AntoniniCastiglia/App.java
+++ AntoniniCastiglia/src/main/java/it/polimi/ingsw/AntoniniCastiglia/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Ora c'è un Paolo felice!");
     }
 }
