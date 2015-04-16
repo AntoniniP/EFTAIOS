b2b88734ec2df8245e414ab5507bd723bd723e07
PaoloAnt <paolo1.antonini@mail.polimi.it>
2015-04-16 15:39:05 +0200

    Test 2.

--- AntoniniCastiglia/src/main/java/it/polimi/ingsw/AntoniniCastiglia/App.java
+++ AntoniniCastiglia/src/main/java/it/polimi/ingsw/AntoniniCastiglia/App.java
@@ -4,11 +4,8 @@ package it.polimi.ingsw.AntoniniCastiglia;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("Ora c'è un Paolo felice!");
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
